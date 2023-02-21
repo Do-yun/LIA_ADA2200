5c4d4d0321f0b0b9474ea61630a565e00be2ccb8
Do-yun <64721158+Do-yun@users.noreply.github.com>
2023-02-21 22:25:32 +0900

    Delete spo2_mod4 directory

--- spo2_mod4/pca10040/s132/ses/Output/Release/Obj/ble_app_hrs_pca10040_s132/ses_nRF_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Release/Obj/ble_app_hrs_pca10040_s132/ses_nRF_Startup.o: \
- C\:\Nordic_SDK\do\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\ses_nRF_Startup.s
--- spo2_mod4/pca10040/s132/ses/Output/ble_app_hrs_pca10040_s132 Release/Obj/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/ble_app_hrs_pca10040_s132\ Release/Obj/sensorsim.o: \
- E:\BLE\nRF52\nRF5_SDK_15.0.0_a53641a\components\libraries\sensorsim\sensorsim.c \
- E:\BLE\nRF52\nRF5_SDK_15.0.0_a53641a\components\libraries\sensorsim\sensorsim.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdbool.h
